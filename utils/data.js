c192311c5ceff0462eb58cce2a9f71b4994cb533
Antonio Gasco <antoniofgasco@gmail.com>
2021-09-27 10:36:03 +0100

    product data and renders

---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'A free shirt'
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/shirt2.jpg',
+      price: 80,
+      brand: 'Adidas',
+      rating: 4.2,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'A fit shirt'
+    },
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/shirt3.jpg',
+      price: 30,
+      brand: 'Nike',
+      rating: 4.9,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'A slim shirt'
+    },
+    {
+      name: 'Golf Pants',
+      category: 'Pants',
+      image: '/images/pants1.jpg',
+      price: 90,
+      brand: 'Oliver',
+      rating: 3.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'Smart looking pants'
+    },
+    {
+      name: 'Fit Pants',
+      category: 'Pants',
+      image: '/images/pants2.jpg',
+      price: 85,
+      brand: 'Zara',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'These are popular pants'
+    },
+    {
+      name: 'Classic Pants',
+      category: 'Pants',
+      image: '/images/pants3.jpg',
+      price: 105,
+      brand: 'Casely',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'These are some slick pants'
+    }
+  ]
+};
+
+export default data;
